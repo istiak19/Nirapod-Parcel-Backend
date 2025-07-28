cdb117b586604e35a5242a09a9e3df03aa6e42ef
istiak19 <istiak15-14128@diu.edu.bd>
2025-07-28 22:36:25 +0600

    project-setup

---
+++ src/app.ts
@@ -0,0 +1,23 @@
+import cors from "cors";
+import express, { Application, Request, Response } from "express";
+
+const app: Application = express();
+
+app.use(express.json());
+app.use(cors({
+    origin: [
+        "http://localhost:5173",
+    ],
+    credentials: true
+}));
+
+// app.use("/api/v1", router);
+
+app.get("/", (req: Request, res: Response) => {
+    res.send("Nirapod Parcel Backend API is running successfully!");
+});
+
+// app.use(globalErrorHandler);
+// app.use(notFound);
+
+export default app;
---
+++ src/server.ts
@@ -0,0 +1,74 @@
+/* eslint-disable no-console */
+import { Server } from "http";
+import mongoose from "mongoose";
+import { envVars } from "./app/config/env.config";
+import app from "./app";
+
+const port = envVars.PORT;
+let server: Server;
+
+async function startServer() {
+    try {
+        await mongoose.connect(envVars.DB_URL);
+        console.log("Connected to MongoDB!")
+        server = app.listen(port, () => {
+            console.log(`Server is running at http://localhost:${port}`)
+        })
+    } catch (error) {
+        console.log(error)
+    }
+};
+
+(async () => {
+    // await redisConnected();
+    await startServer();
+    // await seedSuperAdmin();
+})();
+
+process.on("SIGTERM", () => {
+    console.log("SIGTERM received. Gracefully shutting down...");
+
+    if (server) {
+        server.close(() => {
+            process.exit(1)
+        });
+    }
+
+    process.exit(1)
+})
+
+process.on("SIGINT", () => {
+    console.warn("SIGINT received. Gracefully shutting down...");
+    if (server) {
+        server.close(() => {
+            process.exit(0);
+        });
+    } else {
+        process.exit(0);
+    }
+});
+
+process.on("unhandledRejection", (err) => {
+    console.error("Unhandled Promise Rejection detected. Shutting down the server...");
+    console.error("Error details:", err);
+    if (server) {
+        server.close(() => {
+            process.exit(1)
+        });
+    }
+    process.exit(1)
+});
+
+// Promise.reject(new Error("I forgot to catch this promise"));
+
+process.on("uncaughtException", (err) => {
+    console.error("Uncaught Exception detected. Shutting down the server...");
+    console.error("Error details:", err);
+    if (server) {
+        server.close(() => {
+            process.exit(1);
+        });
+    } else {
+        process.exit(1);
+    }
+});
