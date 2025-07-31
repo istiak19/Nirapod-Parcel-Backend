a3eb22975e9c70313ddf9730d0cd6458d09ce253
istiak19 <istiak15-14128@diu.edu.bd>
2025-07-31 15:06:32 +0600

    tracking part created

--- src/app/modules/parcel/parcel.route.ts
+++ src/app/modules/parcel/parcel.route.ts
@@ -7,6 +7,7 @@ import { parcelController } from "./parcel.controller";
 const router = Router();
 
 router.get("/", checkAuth("Admin"), parcelController.getAllParcel);
+router.get("/track/:trackingId", checkAuth("Admin", "Receiver", "Sender"), parcelController.getTrackingParcel);
 router.post("/", checkAuth("Sender"), validateRequest(createParcelZodSchema), parcelController.createParcel);
 router.get("/me", checkAuth("Sender"), parcelController.getMeParcel);
 router.get("/incoming", checkAuth("Receiver"), parcelController.incomingParcels);
